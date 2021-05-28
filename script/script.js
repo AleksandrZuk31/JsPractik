4c1f7e4ea2ed53726a2dbb2bba4f8ebb601684df
Aleksand Zuk <Aleksandrzuk30013010@gmail.com>
2021-05-28 19:45:54 +0300

    выполнил задание второго урока

--- script/script.js
+++ script/script.js
@@ -1,10 +1,33 @@
-let money;
-let income;
-let addExprenses;
-let deposit;
-let mission;
-let period;
 
-alert(('Hello World!!!'));
 
-console.log('Hello GloAcademy');
+let money, income, addExprenses, deposit, mission, period;
+
+// присваиваю значения
+
+money = 60000;
+income = 'car repairs';
+addExprenses = 'Phone, Petrol, Internet'; 
+deposit = true;
+mission = 1000000;
+period = 12;
+
+// применяю методы и свойства
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExprenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExprenses.toLowerCase());
+console.log(addExprenses.split (', '));
+
+// объявляю пременную ежедневного бюджета и вывожу в консоль 
+
+let budgetDay = money / 30;
+
+console.log(budgetDay);
+
